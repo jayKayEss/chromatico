44c59df39d6a2c3dd02558193fd860639f4ca651
Justin Sheckler <self@jaykayess.com>
2011-10-06 04:33:46 +0000

    different screen sizes, with animation

--- colortoy.js
+++ colortoy.js
@@ -1,28 +1,83 @@
 function Colortoy(canvas) {
     this.snippets = new TextSnippets();
+    this.randColors = null;
+    this.randDrawing = null;
     this.hexchars = "0123456789ABCDEF";
     this.fonts = ['sans', 'sans-serif', 'monospace'];
     this.styles = ['', 'bold', 'italic'];
+    this.sizes = [[500, 500], [320, 480], [640, 960], [1440, 900]];
 
     this.canvas = canvas;
     this.ctx = this.canvas.getContext('2d');
     this.width = canvas.getAttribute('width');
     this.height = canvas.getAttribute('height');
-    this.margin = Math.floor(this.height*.1);
     this.colors = [];
     this.numColors = 0;
     this.ptrColors = 0;
+
+    console.log(this.width+':'+this.height);
 }
 
 Colortoy.prototype = {
+    changeSize: function(n) {
+        var self = this;
+        var tuple = this.sizes[n];
+        var width = tuple[0];
+        var height = tuple[1];
+        var displayWidth;
+        var displayHeight;
+
+        if (width > height) {
+            displayWidth = 500;
+            displayHeight = Math.floor(height * (displayWidth / width));
+        } else {
+            displayHeight = 500;
+            displayWidth = Math.floor(width * (displayHeight / height));
+        }
+
+        $(this.canvas).animate({
+            height: displayHeight,
+            width: displayWidth
+        }, {
+            step: function(now, fx) {
+                if (fx.prop == 'width') {
+                    self.width = Math.floor(now/2);
+                } else {
+                    self.height = Math.floor(now/2);
+                }
+
+                self.canvas.setAttribute('width', self.width);
+                self.canvas.setAttribute('height', self.height);
+                self.redraw();
+            },
+
+            complete: function(){
+                self.width = width;
+                self.height = height;
+                self.canvas.setAttribute('width', width);
+                self.canvas.setAttribute('height', height);
+                self.redraw();
+            }
+        });
+   },
+
     shuffle: function() {
+        this.randColors = new RandomNumbers(100);
         this.populateColors();
         this.compose();
     },
 
-    compose: function() {
+    redraw: function() {
+//        console.log('REDRAW: '+this.width+':'+this.height);
+        this.randDrawing.reset();
+        this.setMargins();
         this.setBackground();
         this.drawText();
+    },
+
+    compose: function() {
+        this.randDrawing = new RandomNumbers(100);
+        this.redraw();
     },
 
     setBackground: function() {
@@ -46,12 +101,14 @@ Colortoy.prototype = {
     drawText: function() {
         var snips = this.randNum(5, 3);
         for (var i=0; i<snips; i++) {
-            var text = this.snippets.getRandom();
+            var text = this.snippets.getRandom(this.randDrawing);
             var color = this.nextColor();
             this.ctx.fillStyle = color;
-            var h = this.getH();
+            var h = this.getTextHeight();
             var x = this.getX();
             var y = this.getY();
+
+//            console.log('TEXT: '+x+','+y+' '+h);
 
             this.ctx.font = this.getFontString(h);
             this.ctx.textBaseline = 'top';
@@ -70,14 +127,14 @@ Colortoy.prototype = {
     },
 
     getFontString: function(h) {
-        var style = this.styles[Math.floor(Math.random() * this.styles.length)];
-        var font = this.fonts[Math.floor(Math.random() * this.fonts.length)];
+        var style = this.styles[Math.floor(this.randDrawing.next() * this.styles.length)];
+        var font = this.fonts[Math.floor(this.randDrawing.next() * this.fonts.length)];
         return style+" "+h+"px "+font;
     },
 
     getX: function() {
         return this.randNum(
-            this.height - this.margin,
+            this.width - this.margin,
             0 - this.margin
         );
     },
@@ -91,8 +148,8 @@ Colortoy.prototype = {
 
     getW: function() {
         return this.randNum(
-            Math.floor(this.height*1.3), 
-            Math.floor(this.height*.1)
+            Math.floor(this.width*1.3), 
+            Math.floor(this.width*.05)
         );
     },
 
@@ -100,15 +157,28 @@ Colortoy.prototype = {
         var min = this.height*.05;
         var max = this.height*1.3;
         return Math.floor(
-            Math.pow(Math.random(), 1.8) * (max-min) + min
+            Math.pow(this.randDrawing.next(), 1) * (max-min) + min
         ); 
+    },
+
+    getTextHeight: function() {
+        var min = this.sqrsize*.05;
+        var max = this.sqrsize*1.3;
+        return Math.floor(
+            Math.pow(this.randDrawing.next(), 1) * (max-min) + min
+        ); 
+    },
+
+    setMargins: function() {
+        this.sqrsize = Math.floor(Math.sqrt(this.width * this.height));
+        this.margin = Math.floor(this.sqrsize*.1);
     },
 
     populateColors: function() {
         $('#theColors').empty();
         this.colors = [];
 
-        this.numColors = this.randNum(5, 3);
+        this.numColors = Math.floor(this.randColors.next() * 2 + 3);
         for (var i=0; i<this.numColors; i+=1) {
             var c = this.genRandColor();
             this.colors[i] = c;
@@ -130,7 +200,7 @@ Colortoy.prototype = {
     randNum: function(max, min) {
         if (min == undefined) min = 1;
         var n = max-min;
-        var ret = Math.round(Math.random() * n + min);
+        var ret = Math.round(this.randDrawing.next() * n + min);
 
         return ret;
     },
@@ -141,19 +211,19 @@ Colortoy.prototype = {
 
         for (var i=0; i<3; i++) {
             var n = Math.floor(
-                Math.pow(Math.random(), .7)
+                Math.pow(this.randColors.next(), 1)
                 * 256
             );
             hex = n.toString(16);
 
             while (hex.length < 2) {
-                console.log(hex);
                 hex = '0' + hex;
             }
 
             ret += hex;
         }
 
+//        console.log(ret);
         return ret;
     },
 
@@ -253,14 +323,47 @@ function TextSnippets() {
 
 TextSnippets.prototype = {
 
-    getRandom: function() {
-        var i = Math.floor(Math.random() * this.text.length);
+    getRandom: function(rand) {
+        var i = Math.floor(rand.next() * this.text.length);
 //        console.log(i+' '+this.text[i]);
         return this.text[i];
     }
 
 };
 
+
+function RandomNumbers(n) {
+    this.numbers = [];
+    this.ptr = 0;
+    this.populate(n);
+};
+
+RandomNumbers.prototype = {
+
+    populate: function(n) {
+        for (var i=0; i<n; i++) {
+            this.numbers[i] = Math.random();
+        }
+
+//        console.log(this.numbers.length);
+    },
+
+    reset: function() {
+        this.ptr = 0;
+    },
+
+    next: function() {
+        if (this.ptr < this.numbers.length) {
+            var ret = this.numbers[this.ptr];
+            this.ptr++;
+            return ret;
+        } else {
+            console.log("Out of numbers!");
+            return Math.random();
+        }
+    }
+
+};
 
 $('document').ready(function(){
     var colortoy = new Colortoy($('#theCanvas').get(0));
@@ -275,6 +378,11 @@ $('document').ready(function(){
 
     $('#recompose').click(function(event){
         colortoy.compose();
+    });
+
+    $('#sizeMenu').change(function(event){
+//        console.log(event);
+        colortoy.changeSize(event.srcElement.value);
     });
 
     colortoy.shuffle();
