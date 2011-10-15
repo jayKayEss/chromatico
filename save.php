ef72657e11f66ae5683ff89ffd4a131757729262
Justin Sheckler <self@jaykayess.com>
2011-10-15 21:08:09 +0000

    Add a watermark to downloads

--- save.php
+++ save.php
@@ -4,8 +4,19 @@ $imagedata = $_POST['imagedata'];
 $imagedata = substr($imagedata, strpos($imagedata, ',')+1);
 $imagedata = base64_decode($imagedata);
 
+$image = imagecreatefromstring($imagedata);
+$watermark = imagecreatefrompng('watermark.png');
+
+imagecopy(
+    $image, $watermark,
+    imagesx($image) - imagesx($watermark), imagesy($image) - imagesy($watermark),
+    0, 0,
+    imagesx($watermark), imagesy($watermark)
+);
+
 header('Content-Type: image/png;base64');
 header('Content-Disposition: attachment; filename=chromatico.png');
 
-print $imagedata;
+imagepng($image);
+imagedestroy($image);
 
